123654ede91a9c1c502f45b27f0176d49ac64e89
iimoon <aymanabdk@gmail.com>
2024-12-14 12:53:04 +0530

    fixed nav import

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";  
-import "./styles/Navbar.css";
+import "./styles/navbar.css";
 import alabbarFMlogo from "../assets/logos/alabbarfm.png";
 import alabbarPhoneLogo from "../assets/logos/ab.png";
 
