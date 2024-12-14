7493a66d5cd2058f99c1d8bb454aa55da2c40cd7
iimoon <aymanabdk@gmail.com>
2024-12-14 21:45:06 +0530

    analytics

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ import { Analytics } from "@vercel/analytics/react"
 function App() {
   return (
     <>
+      <Analytics/>
       <BrowserRouter>
         <ScrollToTop />
         <ToastContainer />
