ffca4cb64ccf57372cebc037ab40d8fb812c3ddd
iimoon <aymanabdk@gmail.com>
2024-12-16 11:41:18 +0530

    facilty page ios fix

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import dotenv from 'dotenv'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true, // Enable external access
   },
+  define:{
+    'process.env.VITE_WEB_3_API':JSON.stringify(process.env.VITE_WEB_3_API)
+  }
 })
